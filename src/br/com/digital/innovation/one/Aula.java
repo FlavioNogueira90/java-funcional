b7319414989d41b9562a735d95d4681003829720
Flavio Nogueira <flavionogueira20@gmail.com>
2021-09-08 16:52:10 -0300

    First commit

---
+++ src/br/com/digital/innovation/one/Aula.java
@@ -0,0 +1,19 @@
+package br.com.digital.innovation.one;
+
+import java.util.function.UnaryOperator;
+
+public class Aula {
+    public static void main(String[] args) {
+        // PARADIGMA IMPERATIVO
+        int valor = 10;
+        int resultado = valor * 3;
+        System.out.println(resultado);
+
+        // mesmo processo com lambda
+        //PARADIGMA FUNCIONAL
+        UnaryOperator<Integer> calcularValorVezestres = valor2 -> valor2*3;
+        int valor2 = 10;
+        System.out.println(calcularValorVezestres.apply(valor2));
+
+    }
+}
